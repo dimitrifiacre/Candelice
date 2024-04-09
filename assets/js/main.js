7aeff404fdefb69c9c04f52709cb0cb202651821
Dimitri Fiacre <dimitri.fcr@gmail.com>
2024-04-09 10:42:31 +0200

    V1 de la nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,35 @@
-window.addEventListener("scroll", function() {
+// CHANGE STYLE MENU ON FIRST SCROLL
+function menuOnScroll() {
     const header = document.querySelector("header");
-    const headerLogo = document.querySelector(".header__logo svg path");
-
     const scrollPosition = window.scrollY;
+    const isScrolled = scrollPosition > 0;
 
-    // Prévoir condition pour dark mode
-    if (scrollPosition > 0) {
-        header.classList.add("scrolled");
-        headerLogo.style.fill = "#F0725C";
+    header.classList.toggle("scrolled", isScrolled);
+}
+
+window.addEventListener("scroll", menuOnScroll);
+
+// MENU ON CLICK
+const navbarToggle = document.getElementById("navbar-toggle");
+const headerNavigation = document.querySelector(".header__navigation");
+const nav = document.querySelector(".header__navigation nav");
+const header = document.querySelector("header");
+
+navbarToggle.addEventListener("click", () => {
+    headerNavigation.classList.toggle("active"); 
+    header.classList.toggle("active");
+
+    if (headerNavigation.classList.contains("active")) {
+        nav.style.top = "0px";
     } else {
-        header.classList.remove("scrolled");
-        headerLogo.style.fill = "#FCFCFC";
+        nav.style.top = "-252px";
+    }
+});
+
+headerNavigation.addEventListener("click", (e) => {
+    if (!e.target.closest("nav")) {
+        headerNavigation.classList.remove("active");
+        header.classList.remove("active");
+        nav.style.top = "-252px";
     }
 });
